bc5dc61dcf5c55be87849654a7b38dd3cf5f522a
Alicja Buchman <emunah73@gmail.com>
2021-04-25 20:49:29 -0400

    adding data class

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,51 @@
 import 'package:flutter/material.dart';
-import './result.dart';
-import './quiz.dart';
+import './timechunk.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() {
-      return _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
   }
-
-
 }
 
-class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
-  var _totalScore = 0;
-  final _questions = const [
-      { 'questionText' : 'How many hours you sleep on average night?', 
-        'answers' : [
-          {'text' : '8h', 'score' : 8*7}, 
-          {'text' : '7h', 'score' : 7*7}, 
-          {'text' : '6h', 'score' : 6*7}, 
-          {'text' : '9h', 'score' : 9*7}, 
-         ],
-      },
-      { 'questionText' : 'How many hours a week you spend at work including commute time?', 
-        'answers' : [
-          {'text' : '40', 'score' : 40},
-          {'text' : '50', 'score' : 50},
-          {'text' : '60', 'score' : 60},
-          {'text' : '30', 'score' : 30}
-        ],
-      },
-      { 'questionText' : 'How many hours a week you spend on household chores?', 
-        'answers' : [
-          {'text' : '10', 'score' : 10},
-          {'text' : '15', 'score' : 15},
-          {'text' : '20', 'score' : 20},
-          {'text' : '25', 'score' : 25},
-        ],
-      },
-    ];
+class MyHomePage extends StatelessWidget {
+final List<Timechunk> justalist = [ Timechunk(
+      id: 0, 
+      title: 'Learning Flutter', 
+      duration: 60, 
+      start: DateTime.now(),
+      category: 'skills')
+  ];
 
-  void _restartQuiz() {
-    setState( () {
-      _questionIndex = 0;
-      _totalScore = 0;
-    });
-  }
-
-  void _answerQuestion(int score) {
-    _totalScore += score;
-    setState(() {
-      _questionIndex = _questionIndex + 1;
-    });
-    
-    print(_questionIndex);
-  }
 
 
   @override
   Widget build(BuildContext context) {
-
-    
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text('Time Master'), ),
-      body: _questionIndex < _questions.length ? 
-      Quiz(
-        answerQuestion: _answerQuestion, 
-        questionIndex: _questionIndex, 
-        questions: _questions)
-        : Result(_totalScore, _restartQuiz),
-    ), );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Time Master'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              color:Colors.blue,
+              child: Text('Chart!'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            color: Colors.teal,
+            child: Text('Main card')
+          )
+        ],
+      )
+    );
   }
 }
---
+++ lib/timechunk.dart
@@ -0,0 +1,10 @@
+class Timechunk {
+  final String title;
+  final String category;
+  final int id;
+  final int duration;
+  final DateTime start;
+
+  Timechunk({this.title, this.id, this.duration, this.start, this.category});
+
+}
